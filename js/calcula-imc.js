83865ebd93c87372b69d9b9c27dfaf2edaa4a73c
Victor Muramoto <victor.muramoto@locaweb.com.br>
2016-04-13 10:33:42 -0300

    aula3 feito - fix_js

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -1,12 +1,14 @@
-var tdPeso = document.getElementById("peso-2");
-var tdAltura = document.getElementById("altura-2");
+var tdPeso = document.getElementById("peso-1").textContent;
+var tdAltura = document.getElementById("altura-1");
 
-var paciente = {peso : document.getElementById("peso-2").textContent,altura : document.getElementById("altura-2").textContent};
+console.log(tdPeso)
+
+var paciente = {peso : document.getElementById("peso-1").textContent,altura : document.getElementById("altura-2").textContent};
 
 if(paciente.altura > 0)
 {
   console.log( paciente.peso / (paciente.altura * paciente.altura));
-  document.getElementsById('imc-2').textContent = ( paciente.peso / (paciente.altura * paciente.altura));
+  document.getElementById('imc-1').textContent = ( paciente.peso / (paciente.altura * paciente.altura));
 }
 else
   console.log('A altura deve ser maior que 0');
