f79374656e239a64731f5d47b68a62273d989424
VictorMuramoto <victor.muramoto@gmail.com>
2016-04-15 00:55:07 -0300

    aula4 done with FOR

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -1,7 +1,5 @@
 var tabela = document.getElementsByClassName("paciente")
-// for(x = 0; x <= tabela.length - 1; x++)
-var x = 0;
-while ( x <= tabela.length - 1 )
+for(x = 0; x <= tabela.length - 1; x++)
 {
     var paciente_tabela = tabela[x];
 
@@ -15,5 +13,4 @@ while ( x <= tabela.length - 1 )
       paciente_tabela.getElementsByClassName('info-imc')[0].textContent = ( paciente.peso / (paciente.altura * paciente.altura));
     else
       console.log('A altura deve ser maior que 0');
-    x++;
 }
