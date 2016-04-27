8d4e03a82416a424c4504b4d512e7c6f6c615a69
Victor Muramoto <victor.muramoto@locaweb.com.br>
2016-04-27 20:08:54 -0300

    pegaImc anonymous function created

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -1,3 +1,4 @@
+
 var tabela = document.getElementsByClassName("paciente")
 for(x = 0; x <= tabela.length - 1; x++)
 {
@@ -6,11 +7,21 @@ for(x = 0; x <= tabela.length - 1; x++)
     var paciente = {
       nome : paciente_tabela.getElementsByClassName("info-nome")[0].textContent,
       peso : paciente_tabela.getElementsByClassName("info-peso")[0].textContent,
-      altura : paciente_tabela.getElementsByClassName("info-altura")[0].textContent
+      altura : paciente_tabela.getElementsByClassName("info-altura")[0].textContent,
+      pegaImc : function() {
+        if(this.altura > 0){
+          paciente_tabela.getElementsByClassName('info-imc')[0].textContent = ( this.peso / (this.altura * this.altura));
+          return ( this.peso / (this.altura * this.altura));
+        }
+        else
+          console.log('A altura deve ser maior que 0');
+      }
     };
 
-    if(paciente.altura > 0)
-      paciente_tabela.getElementsByClassName('info-imc')[0].textContent = ( paciente.peso / (paciente.altura * paciente.altura));
-    else
-      console.log('A altura deve ser maior que 0');
+    console.log(paciente.pegaImc());
+
+    // if(paciente.altura > 0)
+    //   paciente_tabela.getElementsByClassName('info-imc')[0].textContent = ( paciente.peso / (paciente.altura * paciente.altura));
+    // else
+    //   console.log('A altura deve ser maior que 0');
 }
