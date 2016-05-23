8cf5120a34c610c09fb3a6ec36450c34424aaaeb
Victor Muramoto <victor.muramoto@locaweb.com.br>
2016-05-23 19:00:34 -0300

    aula7.8

---
+++ js/adiciona.js
@@ -0,0 +1,22 @@
+var botao= document.querySelector("#adicionar-paciente");
+
+botao.addEventListener("click",function(event){
+  event.preventDefault();
+
+  var newPatient = 	"<tr class='paciente'>"+
+              "<td class='info-nome'>" + document.querySelector("#campo-nome").value + "</td>"+
+              "<td class='info-peso'>" + document.querySelector("#campo-peso").value + "</td>"+
+              "<td class='info-altura'>" + document.querySelector("#campo-altura").value + "</td>"+
+              "<td class='info-imc'></td>"+
+           "</tr>";
+
+  var tabela = document.querySelector("table");
+  tabela.innerHTML = tabela.innerHTML + newPatient;
+
+  document.querySelector("#campo-nome").value = "";
+  document.querySelector("#campo-peso").value = "";
+  document.querySelector("#campo-altura").value = "";
+
+  montaPaciente(newPatient).pegaImc();
+
+});
